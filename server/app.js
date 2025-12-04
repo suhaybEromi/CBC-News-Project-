6cfd09e7f775cc8c5c7e94ce2ef63ffb17da0682
suhayb <suhayb@gmil.com>
2025-12-04 15:36:02 +0300

    start server

--- server/app.js
+++ server/app.js
@@ -1,16 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+import articlesRouter from "./routes/articles.routes.js";
 
 app.use(cors());
 app.use(express.json());
+app.use("/articles", articlesRouter);
 
-app.get("/", (req, res) => console.log("CBC News Project"));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+connectDB();
---
+++ server/routes/articles.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+// import articlesController from "../controllers/articles.controller.js";
+
+router.get("/", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+export default router;
